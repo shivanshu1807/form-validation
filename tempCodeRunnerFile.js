dfb7d0dc9479a0f8ed274829caa0365a19f1a111
Shivanshu Singh <sr5752@srmist.edu.in>
2024-01-04 09:39:20 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
